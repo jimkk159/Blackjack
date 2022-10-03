15c4c6c29fdc3017bda9cdcf6db73bc871daa54e
Jim <jimkk159@gmail.com>
2022-10-04 00:50:42 +0800

    set funciont

---
+++ app.py
@@ -0,0 +1,48 @@
+
+
+def main():
+
+    # Setting Deck
+    deck_num = 0
+    poker_deck = ["A","K","Q","J","10","9","8","7","6","5","4","3","2"]
+    
+
+    # Setting Player
+    player_num = 0
+    player_in_hand = []
+    players_in_hand = []
+    players_money = []
+
+    # Setting Banker
+    banker = []
+    players_in_hand = []
+
+def choice_deck_num():
+    pass
+
+def game():
+    pass
+
+def deal():
+    pass
+
+def check():
+    pass
+
+def shuffle():
+    pass
+
+def bet():
+    pass
+
+def raise_():
+    pass
+
+def stud():
+    pass
+
+def fold():
+    pass
+
+if __name__ == "__main__":
+    main()
