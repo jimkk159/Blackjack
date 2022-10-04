f4374abeaf11f2ae0b7ce4f7a283e89cd36e18a1
Jim <jimkk159@gmail.com>
2022-10-04 08:04:19 +0800

    set initial function

--- app.py
+++ app.py
@@ -17,31 +17,71 @@ def main():
     banker = []
     players_in_hand = []
 
+
+# Game Setting
 def choice_deck_num():
     pass
 
-def game():
+def is_insurance():
     pass
 
-def deal():
+# Game Start
+def bet():
     pass
+
+def shuffle(deck):
+    pass
+
+def deal(to_who, card_num):
+    pass  
 
 def check():
     pass
 
-def shuffle():
-    pass
-
-def bet():
+def ask_insurance():
     pass
 
 def raise_():
+    pass
+
+def fold():
+    pass
+
+def split():
+    pass
+
+def hit():
+    pass
+
+def stand():
+    pass
+
+def double_down():
+    pass
+
+def bust():
+    pass
+
+# Game End
+def judge():
+    pass
+
+def next():
+    pass
+
+def bet():
     pass
 
 def stud():
     pass
 
 def fold():
+    pass
+
+def push():
+    pass
+
+def surrender():
     pass
 
 if __name__ == "__main__":
